ba89c3a29b5312982df50c98bd2d0c6308e39624
jho7535 <jho7535@naver.com>
2024-04-05 22:13:04 +0900

    initialize

---
+++ src/test/java/jho7535/hellospring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jho7535.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
