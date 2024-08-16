126489a46d136ef3d73ec695a3561cbabb20e502
jho7535 <jho7535@naver.com>
2024-08-16 17:46:32 +0900

    learend on 24.08.16

---
+++ src/main/java/jho7535/hellospring/controller/MemberController.java
@@ -0,0 +1,16 @@
+package jho7535.hellospring.controller;
+
+import jho7535.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+}
--- src/main/java/jho7535/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/jho7535/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package jho7535.hellospring.repository;
 
 import jho7535.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
