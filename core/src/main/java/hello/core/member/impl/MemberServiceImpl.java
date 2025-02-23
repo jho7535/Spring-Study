6ca47f18e3ccaf4e64bc28d1fd330123ffecb85c
jho7535 <jho7535@naver.com>
2025-02-23 22:47:30 +0900

    learned on 25.02.23

--- core/src/main/java/hello/core/member/impl/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/impl/MemberServiceImpl.java
@@ -3,11 +3,15 @@ package hello.core.member.impl;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberServiceImpl implements MemberService {
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -23,14 +23,14 @@ public class ConfigurationSingletonTest {
         MemberRepository memberRepository = ac.getBean("memberRepository",
                 MemberRepository.class);
 
-//모두 같은 인스턴스를 참고하고 있다.
+        //모두 같은 인스턴스를 참고하고 있다.
         System.out.println("memberService -> memberRepository = " +
                 memberService.getMemberRepository());
         System.out.println("orderService -> memberRepository = " +
                 orderService.getMemberRepository());
         System.out.println("memberRepository = " + memberRepository);
 
-//모두 같은 인스턴스를 참고하고 있다.
+        //모두 같은 인스턴스를 참고하고 있다.
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
